caa049875021adb33e0ebc265bd855802282a23b
Chris Sarmiento <chriss@mac-chriss.lan>
2019-02-13 23:30:35 +0000

    added ses

---
+++ src/SES.js
@@ -0,0 +1,40 @@
+const AWS = require('aws-sdk')
+
+const errorMessage = require('./utils/error')
+
+class SES {
+  
+  constructor({ region }) {
+    this.sesClient = new AWS.SES({
+      region,
+      apiVersion: '2010-12-01'
+    })
+  }
+
+  async send({ toAddresses, fromAddress, subject, body }) {
+    console.log({ toAddresses, fromAddress, subject, body });
+    const params = {
+      Destination: { ToAddresses: toAddresses },
+      Message: {
+        Body: {
+          Text: {
+            Data: body,
+          }
+        },
+        Subject: {
+          Data: subject,
+        }
+      },
+      Source: fromAddress,
+    }
+    const data = await new Promise(resolve => {
+      return this.sesClient.sendEmail(params, (error, data) => {
+        if (error) resolve(errorMessage({ source: SES.name, error, method: 'send', item: params }))
+        resolve(data)
+      })
+    })
+    return data
+  }
+}
+
+module.exports = SES
