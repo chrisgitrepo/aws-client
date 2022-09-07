53bc5f59cc7fee1fc391c422fac8438966c617e5
Christopher Sarmiento <chrissarmiento@hotmail.com>
2022-09-07 19:04:39 +0100

    remove old packages that depend on aws-sdk

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
-const DynamoClient = require('./DynamoClient')
-const SNSClient = require('./SNSClient')
-const SES = require('./SES')
 const S3Client = require('./S3Client')
 
 module.exports = {
-  DynamoClient,
-  SNSClient,
-  SES,
   S3Client
 }
